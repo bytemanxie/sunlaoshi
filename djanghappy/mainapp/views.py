fc4d2c2768eff5b141f3c7e143dc4cdb1614f405
xzj <783638806@qq.com>
2024-01-30 16:23:36 +0800

    修改目录

--- djanghappy/mainapp/views.py
+++ djanghappy/mainapp/views.py
@@ -8,4 +8,4 @@ def lunbo(request):
     return render(request, 'mainpage/swiper4-code/index.html')
 
 def holiday(request):
-    return render(request, f'mainpage\home-master\HolidayCountdown-master\src\index.html')
+    return render(request, 'mainpage/home-master/HolidayCountdown-master/src/index.html')
